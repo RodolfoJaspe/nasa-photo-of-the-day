42626f4476a195bbd8ca6ba5a31cc20fc340e197
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2021-01-05 23:06:44 -0500

    Added date dropdown functionality

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ import axios from 'axios';
 
 function App() {
     const [photoData, setPhotoData] = useState({});
+    const [date, setDate] = useState("");
+
+    let dateHandler = (e) => {
+            setDate(e.target.value);
+        }
+
     useEffect (() => {
-       axios.get("https://api.nasa.gov/planetary/apod?api_key=jOev6Fm1lVwGGzhDnZ9rPdOFWmfmhfUGzBa3TWtL")
+       axios.get(`https://api.nasa.gov/planetary/apod?api_key=jOev6Fm1lVwGGzhDnZ9rPdOFWmfmhfUGzBa3TWtL&date=${date}`)
     .then(response => {
-        console.log(response.data)
         setPhotoData(response.data);
     })
     .catch(err => console.log(`=== ERROR === ${err}`)) 
-    },[])
-    console.log(photoData)
-    
+    },[date])
 
   return (
     <div className="App">
       < Header photoUrl={photoData.hdurl}/>
-      < Photo photoData={photoData}/>
+      < Photo photoData={photoData} dateHandler={dateHandler}/>
       < Footer photoUrl={photoData.hdurl}/>
     </div>
   );
--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './Photo.css'
 
 export default function (props) {
-    console.log(props)
-    const {photoData} = props
+    const {photoData, dateHandler} = props
     return (
         <div className="main">
             <h2>{photoData.title}</h2>
-            <img src={photoData.hdurl} />
+            <img src={photoData.url} alt="NASA POTD"/>
             <div className="info">
                 <p>{photoData.explanation}</p>
                 <h3>{photoData.date}</h3>
+                <form>
+                    <input type="date" onChange={dateHandler}/>
+                </form>
             </div>
             
         </div>
